7c352c804ccf236ce8c227bc44a3537490a56b63
Dirk Arnez <smalldirkalex@gmail.com>
2024-03-04 00:25:00 +0800

    - add local-build.sh

---
+++ local-build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export PATH="/mingw64/bin:/usr/local/bin:/usr/bin:/bin:$USERPROFILE/Downloads"
+export PATH="$PATH:$USERPROFILE/Downloads/xpack-aarch64-none-elf-gcc-13.2.1-1.1-win32-x64/xpack-aarch64-none-elf-gcc-13.2.1-1.1/bin"
+# export PATH="$PATH:$USERPROFILE/Downloads/gcc-arm-none-eabi-10-2020-q4-major/bin"
+export MAKE="$USERPROFILE/Downloads/x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/bin/mingw32-make.exe"
+
+make="${MAKE:-make}"
